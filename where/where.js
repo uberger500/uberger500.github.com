988f78f48803ca1ab99c796ab70166cba104694c
uberger500 <uberger500@gmail.com>
2013-03-03 09:17:05 -0500

    test for distcalc2

--- where/where.js
+++ where/where.js
@@ -163,6 +163,8 @@ console.log("inwaldoDist3");
         	Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	var d = R * c;
+console.log("inwaldoDist4");
+console.log(d);
 	return d;
 }
 
